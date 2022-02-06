3251f3b1aeaf7d7fd86c0590528ce5741e3a4293
Marin Ivanov <dev@metala.org>
2022-02-06 10:40:50 +0200

    clone.sh: Add NJOBS with default $(nproc) and STOP_NAME

--- clone.sh
+++ clone.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 FILENAME="$1"
+STOP_NAME="$2"
+shift 2
+
+NJOBS="${NJOBS:-$(nproc)}"
 
 if [ -z "$FILENAME" -o ! -f "$FILENAME" ]; then
 	echo >&2 "Missing GitHub stars json filename."
@@ -11,5 +15,11 @@ for row in $(jq -c '.[] | {name, url}' "$FILENAME"); do
 	NAME="$(echo "$row" | jq -r '.name')"
 	URL="$(echo "$row" | jq -r '.url')"
 
-	git clone -j4 -n --recursive "$URL" "$NAME"
+	echo "# $NAME"
+	if [ -n "$STOP_NAME" -a "$NAME" = "$STOP_NAME" ]; then
+		break
+	fi
+
+	git clone -j "${NJOBS:-1}" -n --recursive "$URL" "$NAME"
 done
+echo "> End"
